4afc5b9ce5e54b734ed6e457338b392d81446c8f
Ben Stovold <bstovold@gmail.com>
2011-10-16 11:22:34 +0100

    Initial Commit

---
+++ TPScrollBarController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TPScrollBarController
+//
+//  Created by Ben Stovold on 16/10/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TPScrollBarController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TPScrollBarController
+//
+//  Created by Ben Stovold on 16/10/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
